a7b4d7afd0c6e3f75ed2a15ad7842135fe2b9104
navidesma <esma.navid@gmail.com>
2024-08-01 00:55:36 +0330

    added navbar, added mobile condition to table component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Container } from "@mui/material";
+import Navbar from "../Navbar/Navbar";
 
 export default function Main({ children }: { children: React.ReactNode }) {
-    return <Container maxWidth='xl'>{children}</Container>;
+    return (
+        <>
+            <Navbar />
+            <Container maxWidth='xl' sx={{ marginTop: "3rem" }}>
+                {children}
+            </Container>
+        </>
+    );
 }
--- src/components/TableComponent/TableComponent.tsx
+++ src/components/TableComponent/TableComponent.tsx
@@ -7,6 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Box, Card, Typography } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -34,6 +35,43 @@ interface TableComponentProps<T> {
 }
 
 export default function TableComponent<T>({ columns, items }: TableComponentProps<T>) {
+    const [isMobileDevice, setIsMobileDevice] = React.useState(window.innerWidth <= 600);
+
+    React.useEffect(() => {
+        const handleWindowReSize = () => {
+            setIsMobileDevice(window.innerWidth <= 600);
+        };
+
+        window.addEventListener("resize", handleWindowReSize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowReSize);
+        };
+    }, []);
+
+    if (isMobileDevice) {
+        return (
+            <Box>
+                {items.map((item) => (
+                    <Card
+                        sx={{
+                            padding: "2rem",
+                            marginTop: "2rem",
+                            boxShadow: "1px 2px 6px",
+                        }}
+                    >
+                        {columns.map((column) => (
+                            <Typography mt={1}>
+                                {column.label ? column.label + ": " : ""}
+                                {column.content(item)}
+                            </Typography>
+                        ))}
+                    </Card>
+                ))}
+            </Box>
+        );
+    }
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 700 }} aria-label='customized table'>
